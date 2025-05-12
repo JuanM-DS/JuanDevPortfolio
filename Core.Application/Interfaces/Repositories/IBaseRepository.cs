c1f126f77dcdcf2fe52b654456f630ac4c7a9b6b
JuanM-DS <20230676@itla.edu.do>
2025-05-12 07:49:21 -0400

    --hotadd

---
+++ Core.Application/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Core.Application.Interfaces.Repositories
+{
+	public interface IBaseRepository<TEntity>
+		where TEntity : BaseEntity
+	{
+		public Task<bool> CreateAsync(TEntity entity);
+		public Task<bool> UpdateAsync(TEntity entity);
+		public Task<bool> DeleteAsync(Guid id);
+		public Task<TEntity?> GetByIdAsync(Guid id);
+		public IEnumerable<TEntity> GetAll();
+		public Task<TEntity?> GetByIdAsync(Guid id, params Expression<Func<TEntity, object>> [] properties);
+		public IEnumerable<TEntity> GetAll(params Expression<Func<TEntity, object>>[] properties);
+	}
+}
