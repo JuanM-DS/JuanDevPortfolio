f8899cfae7d5468d6955a61e17ef5324800c913e
Juan De Los Santos <jmsantos@promerica.com.do>
2025-05-13 09:02:47 -0400

    add base service

---
+++ Core.Application/Interfaces/Services/IBaseServices.cs
@@ -0,0 +1,25 @@
+﻿using Core.Application.Wrappers;
+using Core.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Core.Application.Interfaces.Services
+{
+    public interface IBaseServices<TEntity, TEntityDto>
+        where TEntity : BaseEntity
+        where TEntityDto : class
+    {
+        public Task<AppResponse<TEntityDto>> CreateAsync(TEntityDto saveDto);
+
+        public Task<AppResponse<TEntityDto>> UpdateAsync(TEntityDto saveDto);
+
+        public Task<AppResponse<Guid>> DeleteAsync(Guid Id);
+
+        public Task<AppResponse<TEntityDto?>> GetByIdAsync(Guid Id);
+
+        public AppResponse<List<TEntityDto>> GetAll();
+
+        public AppResponse<List<TEntityDto>> GetAllWithInclude(params Expression<Func<TEntity, object>> [] properties);
+
+        public Task<AppResponse<TEntityDto?>> GetByIdAsync(Guid Id, params Expression<Func<TEntity, object>>[] properties);
+    }
+}
---
+++ Core.Application/Services/BaseServices.cs
@@ -0,0 +1,55 @@
+﻿using Core.Application.Interfaces.Repositories;
+using Core.Application.Interfaces.Services;
+using Core.Application.Wrappers;
+using Core.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Core.Application.Services
+{
+    public class BaseServices<TEntity, TEntityDto> : IBaseServices<TEntity, TEntityDto>
+        where TEntity : BaseEntity
+        where TEntityDto : class
+    {
+        private readonly IBaseRepository<TEntity> _repo;
+
+        public BaseServices(IBaseRepository<TEntity> repo)
+        {
+            _repo = repo;
+        }
+
+        public Task<AppResponse<TEntityDto>> CreateAsync(TEntityDto saveDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppResponse<Guid>> DeleteAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AppResponse<List<TEntityDto>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AppResponse<List<TEntityDto>> GetAllWithInclude(params Expression<Func<TEntity, object>>[] properties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppResponse<TEntityDto?>> GetByIdAsync(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppResponse<TEntityDto?>> GetByIdAsync(Guid Id, params Expression<Func<TEntity, object>>[] properties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppResponse<TEntityDto>> UpdateAsync(TEntityDto saveDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
