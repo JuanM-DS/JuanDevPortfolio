e3c5c0bf3d8df0d22ea10832837085edbba71b38
Juan De Los Santos <jmsantos@promerica.com.do>
2025-05-14 11:03:14 -0400

    --hotfix mapper

--- Core.Application/Interfaces/Helpers/IMapper.cs
+++ Core.Application/Interfaces/Helpers/IMapper.cs
@@ -3,6 +3,5 @@
     public interface IMapper
     {
         public TResult? Handler<TResult, TSource>(TSource source);
-        public void AddMappingProfile<TResult, TSource>(Func<object, object> func);
     }
 }
